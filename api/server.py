796a4ae67ed88f55d2469224d5fc71800c1fb3d0
Brandon Faunce <masterbbud@gmail.com>
2024-01-31 20:41:08 -0500

    Added gcloud_key (gitignored

--- api/server.py
+++ api/server.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI, Request, UploadFile, File, Form, HTTPException
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate('path/to/serviceAccount.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
