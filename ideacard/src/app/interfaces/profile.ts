01125ec7e8636ed11088d0c0d89bba7b1a513a28
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2024-02-09 22:39:19 -0500

    updated ng interfaces

--- ideacard/src/app/interfaces/profile.ts
+++ ideacard/src/app/interfaces/profile.ts
@@ -3,6 +3,10 @@ import { Testimonial } from "./testimonial";
 
 export interface Profile {
     name: string,
+    language: string,
+    location: string,
+    uuid: string,
+    publicprofile: boolean,
     disabilities: Disability[],
     testimonials: Testimonial[]
 }
