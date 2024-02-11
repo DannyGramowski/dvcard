189bbf91e8f754a60d154602187b95554e17ef04
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2024-02-11 15:52:37 -0500

    fixed profile and added public profile endpoints

--- api/server.py
+++ api/server.py
@@ -430,14 +430,21 @@ def get_profile(Id_Token: Annotated[str | None, Header()] = None):
         return {'name': 'test', 'exists': True, 'disabilities': [{'id': 0, 'name': 'Disability Name', 'description': 'This is an example of a disability', 'symptoms': [{'id': 0, 'name': 'Symptom 1', 'description': 'Test Description'}], 'accommodations': []}]*2, 'testimonials': []}
         #return doc_ref[1]
     
+    return {"user": get_user(user_id), DISABILITIES: get_disabilities(user_id), TESTIMONIALS: get_testimonials(user_id)}
+
+@app.get("/publicprofile")
+def public_get_profile(user_id: str):
+    
+    doc_ref = get_doc([(USERS, user_id)])
+    if doc_ref[0] is False:
+        #return {'name': 'test', 'exists': True, 'disabilities': [{'id': 0, 'name': 'Disability Name', 'description': 'This is an example of a disability', 'symptoms': [{'id': 0, 'name': 'Symptom 1', 'description': 'Test Description'}], 'accommodations': []}]*2, 'testimonials': []}
+        return doc_ref[1]
+    
     user = doc_ref[1].get().to_dict()
     if user["publicprofile"] is True: #return all info
         return {"user": get_user(user_id), DISABILITIES: get_disabilities(user_id), TESTIMONIALS: get_testimonials(user_id)}
     else: # return name
         return {"user": get_user(user_id)["name"]}
-
-def public_get_profile(uuid: str):
-    pass
 
 def generate_uuid_from_ref(ref):
     """
