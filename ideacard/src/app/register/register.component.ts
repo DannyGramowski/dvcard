cf3b3d1c537d1540e1e5fd861d0085296c95a11b
DannyGramowski <57734302+DannyGramowski@users.noreply.github.com>
2024-02-11 22:25:47 -0500

    working registration

--- ideacard/src/app/register/register.component.ts
+++ ideacard/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 
 @Component({
   selector: 'app-register',
@@ -8,5 +10,55 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  url:string = "http://127.0.0.1:8000";
 
+
+  loginUser(authToken: string) : void{
+    console.log(authToken);
+    let headers = new Headers();
+    headers.set('Id-Token', authToken);
+
+  }
+
+  register() {
+    let name:string = (<HTMLInputElement>document.getElementById("nameInput"))?.value
+    let email:string = (<HTMLInputElement>document.getElementById("emailInput"))?.value
+    let password:string = (<HTMLInputElement>document.getElementById("passwordInput"))?.value
+    let reenterpassword:string = (<HTMLInputElement>document.getElementById("reenterPasswordInput"))?.value
+    let country:string = (<HTMLInputElement>document.getElementById("countryInput"))?.value
+    let selection = <HTMLSelectElement>document.getElementById("languageSelector")
+    let language:string = selection.options[selection.selectedIndex]?.value
+    const auth = getAuth()
+
+    if(password !== reenterpassword) {
+      console.log("passwords not equal");
+      return
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(credentials => {
+      credentials.user.getIdToken()
+      .then(result => {
+        this.loginUser(result) 
+        let headers = new Headers();
+        headers.set('Id-Token', result)
+        headers.set("Content-Type", 'application/json')
+        fetch(`${this.url}/login`, {headers: headers})
+        .then( () => {
+          fetch(`${this.url}/user`, 
+            {method: 'PUT', headers: headers, 
+              body: JSON.stringify({
+                name: name,
+                location: country,
+                language: language
+              })
+            }
+          )
+        })
+      })
+    })
+    .catch(error => console.log(error));
+    //fetch(`${url}/testauth?email=${this.email}&password=${this.password}`)
+    
+  }
 }
